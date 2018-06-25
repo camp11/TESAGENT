42624cd58be2a40de39daebf57ad3aff11397134
camp11 <30381042+camp11@users.noreply.github.com>
2018-06-25 07:28:49 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,597 @@
+<?php
+/*
+pyupyu
+*/
+
+require_once('./line_class.php');
+require_once('./unirest-php-master/src/Unirest.php');
+
+$channelAccessToken = 'zl4h/XdHZN0fK4R1IJudjKWN5ikQPOItMfGGeiCVQbkH1wFuYUsMaRWD+iMy/H1owU3TeFGJ2xrstCFCW0BP3k2lBl6OcX/QFKQaZJ0IjdsibUgCDlFVtds10dbEvNqSf3KC216bcC2oYaeM5K5TuQdB04t89/1O/w1cDnyilFU='; //sesuaikan 
+$channelSecret = '9c5a106927970e96047236eea3824b62';//sesuaikan
+
+$client = new LINEBotTiny($channelAccessToken, $channelSecret);
+
+$userId 	= $client->parseEvents()[0]['source']['userId'];
+$groupId 	= $client->parseEvents()[0]['source']['groupId'];
+$replyToken = $client->parseEvents()[0]['replyToken'];
+$timestamp	= $client->parseEvents()[0]['timestamp'];
+$type 		= $client->parseEvents()[0]['type'];
+
+$message 	= $client->parseEvents()[0]['message'];
+$messageid 	= $client->parseEvents()[0]['message']['id'];
+
+$profil = $client->profil($userId);
+
+$pesan_datang = explode(" ", $message['text']);
+$msg_type = $message['type'];
+$command = $pesan_datang[0];
+$options = $pesan_datang[1];
+if (count($pesan_datang) > 2) {
+    for ($i = 2; $i < count($pesan_datang); $i++) {
+        $options .= '+';
+        $options .= $pesan_datang[$i];
+    }
+}
+#-------------------------[Function]-------------------------#
+//show menu, saat join dan command,menu
+if ($type == 'join' || $command == 'Wc') {
+    $text .= "====[HALLO WELCOME]====";
+    $text .= " \n";
+    $text .= "       ⤵Selamat Datang di⤵\n";
+    $text .= "=======================\n";	
+    $text .= "      >ⓜⓐⓘⓝ ⓡⓞⓞⓜ<\n";	
+    $text .= "BIRTHDAY EVENT ANNE - 27th\n";
+    $text .= "=======================\n";	
+    $text .= "  Jangan Lupa Cek Note ya\n";
+    $text .= "[Salken dari Saya]->$profil->displayName\n";
+    $balas = array(
+        'replyToken' => $replyToken,
+        'messages' => array(
+            array(
+                'type' => 'text',
+                'text' => $text
+            )
+        )
+    );
+}
+//pesan bergambar
+if($message['type']=='text') {
+	    if ($command == 'Pagi' || $command == 'pagi' ) {
+
+        $balas = array(
+            'replyToken' => $replyToken,
+            'messages' => array(
+                array(
+                    'type' => 'text',
+                    'text' => 'Pagi juga, Senyum ya! '.$profil->displayName
+                )
+            )
+        );
+    }
+}
+//pesan bergambar
+if($message['type']=='text') {
+	    if ($command == 'Siang' || $command == 'siang' ) {
+
+        $balas = array(
+            'replyToken' => $replyToken,
+            'messages' => array(
+                array(
+                    'type' => 'text',
+                    'text' => 'Siang juga, Jangan lupa makan siang ya '.$profil->displayName
+                )
+            )
+        );
+    }
+}
+//pesan bergambar
+if($message['type']=='text') {
+	    if ($command == 'Assalamualaikum' || $command == 'assalamualaikum' ) {
+
+        $balas = array(
+            'replyToken' => $replyToken,
+            'messages' => array(
+                array(
+                    'type' => 'text',
+                    'text' => 'waalaikumsalam wr.wb '.$profil->displayName
+                )
+            )
+        );
+    }
+}
+//pesan bergambar
+if($message['type']=='text') {
+	    if ($command == 'Waalaikumsalam' || $command == 'Waalaikumsalam wr.wb' ) {
+
+        $balas = array(
+            'replyToken' => $replyToken,
+            'messages' => array(
+                array(
+                    'type' => 'text',
+                    'text' => 'Makasih dah jawab salamnya kk '.$profil->displayName
+                )
+            )
+        );
+    }
+}
+//pesan bergambar
+if($message['type']=='text') {
+	    if ($command == 'Sore' || $command == 'sore' ) {
+
+        $balas = array(
+            'replyToken' => $replyToken,
+            'messages' => array(
+                array(
+                    'type' => 'text',
+                    'text' => 'Ngopi dulu '.$profil->displayName
+                )
+            )
+        );
+    }
+}
+//pesan bergambar
+if($message['type']=='text') {
+	    if ($command == 'Malam' || $command == 'Night' ) {
+
+        $balas = array(
+            'replyToken' => $replyToken,
+            'messages' => array(
+                array(
+                    'type' => 'text',
+                    'text' => 'Malam juga, '.$profil->displayName
+                )
+            )
+        );
+    }
+}
+//pesan bergambar
+if($message['type']=='text') {
+	    if ($command == 'baik' || $command == 'Baik' ) {
+
+        $balas = array(
+            'replyToken' => $replyToken,
+            'messages' => array(
+                array(
+                    'type' => 'text',
+                    'text' => 'Tetap Semangat Ya! '.$profil->displayName
+                )
+            )
+        );
+    }
+}
+//pesan bergambar
+if($message['type']=='text') {
+	    if ($command == 'Halo' || $command == 'Hallo' ) {
+
+        $balas = array(
+            'replyToken' => $replyToken,
+            'messages' => array(
+                array(
+                    'type' => 'text',
+                    'text' => 'HALLO apa kabar'.$profil->displayName
+                )
+            )
+        );
+    }
+}
+//pesan bergambar
+if($message['type']=='text') {
+	    if ($command == 'Hai' || $command == 'hai' ) {
+
+        $balas = array(
+            'replyToken' => $replyToken,
+            'messages' => array(
+                array(
+                    'type' => 'text',
+                    'text' => 'Hai juga '.$profil->displayName
+                )
+            )
+        );
+    }
+}
+//pesan bergambar
+if($message['type']=='text') {
+	    if ($command == 'Udh' || $command == 'udh' ) {
+
+        $balas = array(
+            'replyToken' => $replyToken,
+            'messages' => array(
+                array(
+                    'type' => 'text',
+                    'text' => 'pinter kamu '.$profil->displayName
+                )
+            )
+        );
+    }
+}
+//pesan bergambar
+if($message['type']=='text') {
+	    if ($command == 'Udah' || $command == 'udah' ) {
+
+        $balas = array(
+            'replyToken' => $replyToken,
+            'messages' => array(
+                array(
+                    'type' => 'text',
+                    'text' => 'pinter kamu '.$profil->displayName
+                )
+            )
+        );
+    }
+}
+//pesan bergambar
+if($message['type']=='text') {
+	    if ($command == 'Ok' || $command == 'ok' ) {
+
+        $balas = array(
+            'replyToken' => $replyToken,
+            'messages' => array(
+                array(
+                    'type' => 'text',
+                    'text' => 'pinter kamu '.$profil->displayName
+                )
+            )
+        );
+    }
+}
+//pesan bergambar
+if($message['type']=='text') {
+	    if ($command == 'gila' || $command == 'peak' ) {
+
+        $balas = array(
+            'replyToken' => $replyToken,
+            'messages' => array(
+                array(
+                    'type' => 'text',
+                    'text' => 'ish ish kok gitu ka( '.$profil->displayName
+                )
+            )
+        );
+    }
+}
+//pesan bergambar
+if($message['type']=='text') {
+	    if ($command == 'Done5' || $command == 'DONE5' ) {
+
+        $balas = array(
+            'replyToken' => $replyToken,
+            'messages' => array(
+                array (
+  'type' => 'template',
+  'altText' => 'Min OPEN Vote YES/NO',
+  'template' => 
+  array (
+    'type' => 'confirm',
+    'text' => 'Terima Kasih & Selamat Datang kaka..Selamat Bergabung Menjadi Bagian Dari Keluarga FIS (*Admin silahkan berikan penilaian)',
+    'actions' => 
+    array (
+      0 => 
+      array (
+        'type' => 'message',
+        'label' => 'Yes',
+        'text' => 'yes',
+      ),
+      1 => 
+      array (
+        'type' => 'message',
+        'label' => 'No',
+        'text' => 'no',
+      ),
+    ),
+  ),
+)
+            )
+        );
+    }
+}
+//pesan bergambar
+if($message['type']=='text')
+	if ($command == 'admin' || $command == 'Admin' )
+	{
+		
+		
+		$balas = array(
+							'replyToken' => $replyToken,														
+							'messages' => array(
+array (
+  'type' => 'template',
+  'altText' => 'FIS MANAGEMENT',
+  'template' => 
+  array (
+    'type' => 'carousel',
+    'columns' => 
+    array (
+      0 => 
+      array (
+        'thumbnailImageUrl' => 'https://preview.ibb.co/gDpnMb/20180108_110257.jpg',
+        'imageBackgroundColor' => '#FFFFFF',
+        'title' => 'FOUNDER',
+        'text' => 'Group Owner -- Name : FahreziLee   Location : Malaysia',
+        'actions' => 
+        array (
+          0 => 
+          array (
+            'type' => 'uri',
+            'label' => 'CHAT',
+            'uri' => 'http://tiny.cc/FIS_Lee',
+          ),
+          1 => 
+          array (
+            'type' => 'uri',
+            'label' => 'SMULE',
+            'uri' => 'https://www.smule.com/FIS_FahreziLee',
+          ),		  
+          2 => 
+          array (
+            'type' => 'message',
+            'label' => 'view detail',
+            'text' => 'FIS_LEE',
+          ),
+        ),
+      ),
+      1 => 
+      array (
+        'thumbnailImageUrl' => 'https://preview.ibb.co/gUFu1b/20180108_110910.jpg',
+        'imageBackgroundColor' => '#FFFFFF',
+        'title' => 'SECRETARY',
+        'text' => 'Admin -- Name : DeeAna       Location : Borneo',
+        'actions' => 
+        array (
+          0 => 
+          array (
+            'type' => 'uri',
+            'label' => 'CHAT',
+            'uri' => 'http://tiny.cc/FIS_DEE',
+          ),
+          1 => 
+          array (
+            'type' => 'uri',
+            'label' => 'SMULE',
+            'uri' => 'https://www.smule.com/FIS_Dee',
+          ),		  
+          2 => 
+          array (
+            'type' => 'message',
+            'label' => 'view detail',
+            'text' => 'FIS_Dee',
+          ),
+        ),
+      ),
+      2 => 
+      array (
+        'thumbnailImageUrl' => 'https://preview.ibb.co/njD3uw/20180108_111546.jpg',
+        'imageBackgroundColor' => '#FFFFFF',
+        'title' => 'CREATIVE',
+        'text' => 'Admin -- Name : ALS                 Location : West Java',
+        'actions' => 
+        array (
+          0 => 
+          array (
+            'type' => 'uri',
+            'label' => 'CHAT',
+            'uri' => 'http://tiny.cc/FIS_ALS',
+          ),
+          1 => 
+          array (
+            'type' => 'uri',
+            'label' => 'SMULE',
+            'uri' => 'https://www.smule.com/FIS_ALS',
+          ),		  
+          2 => 
+          array (
+            'type' => 'message',
+            'label' => 'view detail',
+            'text' => 'FIS_ALS',
+          ),
+        ),
+      ),
+      3 => 
+      array (
+        'thumbnailImageUrl' => 'https://preview.ibb.co/nxZySG/20180108_111027.jpg',
+        'imageBackgroundColor' => '#FFFFFF',
+        'title' => 'RESOURCE',
+        'text' => 'Admin -- Name : Lala              Location : Hongkong',
+        'actions' => 
+        array (
+          0 => 
+          array (
+            'type' => 'uri',
+            'label' => 'CHAT',
+            'uri' => 'http://tiny.cc/FIS_LALA',
+          ),
+          1 => 
+          array (
+            'type' => 'uri',
+            'label' => 'SMULE',
+            'uri' => 'https://www.smule.com/FIS_LALA',
+          ),		  
+          2 => 
+          array (
+            'type' => 'message',
+            'label' => 'view detail',
+            'text' => 'FIS_LALA',
+          ),
+        ),
+      ),
+      4 => 
+      array (
+        'thumbnailImageUrl' => 'https://preview.ibb.co/gRYKZw/20180108_111446.jpg',
+        'imageBackgroundColor' => '#FFFFFF',
+        'title' => 'HOME AS.',
+        'text' => 'Admin -- Name : Alfariz              Location : Jakarta',
+        'actions' => 
+        array (
+          0 => 
+          array (
+            'type' => 'uri',
+            'label' => 'CHAT',
+            'uri' => 'http://tiny.cc/FIS_ALFARIZ',
+          ),
+          1 => 
+          array (
+            'type' => 'uri',
+            'label' => 'SMULE',
+            'uri' => 'https://www.smule.com/FIS_Alfariz',
+          ),		  
+          2 => 
+          array (
+            'type' => 'message',
+            'label' => 'view detail',
+            'text' => 'FIS_Alfariz',
+          ),
+        ),
+      ),
+      5 => 
+      array (
+        'thumbnailImageUrl' => 'https://preview.ibb.co/npK41b/20180108_111333.jpg',
+        'imageBackgroundColor' => '#FFFFFF',
+        'title' => 'HOME AS.',
+        'text' => 'Admin -- Name : Juna Hermanza   Location : West Java',
+        'actions' => 
+        array (
+          0 => 
+          array (
+            'type' => 'uri',
+            'label' => 'CHAT',
+            'uri' => 'http://tiny.cc/FIS_JUNA',
+          ),
+          1 => 
+          array (
+            'type' => 'uri',
+            'label' => 'SMULE',
+            'uri' => 'https://www.smule.com/FIS_Juna',
+          ),		  
+          2 => 
+          array (
+            'type' => 'message',
+            'label' => 'view detail',
+            'text' => 'FIS_Juna',
+          ),
+        ),
+      ),
+      6 => 
+      array (
+        'thumbnailImageUrl' => 'https://preview.ibb.co/edtxMb/20180108_111247.jpg',
+        'imageBackgroundColor' => '#FFFFFF',
+        'title' => 'HOME AS.',
+        'text' => 'Admin -- Name : Anissa              Location : West Java',
+        'actions' => 
+        array (
+          0 => 
+          array (
+            'type' => 'uri',
+            'label' => 'CHAT',
+            'uri' => 'http://tiny.cc/FIS_NISA',
+          ),
+          1 => 
+          array (
+            'type' => 'uri',
+            'label' => 'SMULE',
+            'uri' => 'https://www.smule.com/FIS_Nisa',
+          ),		  
+          2 => 
+          array (
+            'type' => 'message',
+            'label' => 'view detail',
+            'text' => 'FIS_Nisa',
+          ),
+        ),
+      ),
+    ),
+    'imageAspectRatio' => 'rectangle',
+    'imageSize' => 'cover',
+  ),
+)
+							)
+						);
+				
+	}
+//pesan bergambar
+if($message['type']=='text') {
+	    if ($command == 'Welcome' || $command == 'wc' ) {
+
+        $balas = array(
+            'replyToken' => $replyToken,
+            'messages' => array(
+                array (
+  'type' => 'imagemap',
+  'baseUrl' => 'https://res.cloudinary.com/tes5566/image/upload/v1524552956/line/Bot/Example',
+  'altText' => 'WELCOME TO FIS FAMILY',
+  'baseSize' => 
+  array (
+    'height' => 1040,
+    'width' => 1040,
+  ),
+  'actions' => 
+  array (
+    0 => 
+    array (
+      'type' => 'uri',
+      'linkUri' => 'https://www.smule.com/FIS_OFFICIAL',
+      'area' => 
+      array (
+        'x' => 0,
+        'y' => 0,
+        'width' => 520,
+        'height' => 1040,
+      ),
+    ),
+    1 => 
+    array (
+      'type' => 'message',
+      'text' => 'Admin',
+      'area' => 
+      array (
+        'x' => 520,
+        'y' => 0,
+        'width' => 520,
+        'height' => 1040,
+      ),
+    ),
+  ),
+)
+            )
+        );
+    }
+}
+//pesan bergambar
+if($message['type']=='text') {
+	    if ($command == 'Creator' || $command == 'creator' ) {
+        $balas = array(
+            'replyToken' => $replyToken,
+            'messages' => array(
+                array (
+  'type' => 'template',
+  'altText' => 'CREATOR',
+  'template' => 
+  array (
+    'type' => 'image_carousel',
+    'columns' => 
+    array (
+      0 => 
+      array (
+        'imageUrl' => 'https://res.cloudinary.com/eds0101/image/upload/v1527926484/Creator/1040.jpg',
+        'action' => 
+        array (
+          'type' => 'uri',
+          'label' => 'CHAT PM',
+          'uri' => 'http://line.me/ti/p/8jX6OIm-AS',
+        ),
+      ),
+    ),
+  ),
+)
+            )
+        );
+    }
+}
+if (isset($balas)) {
+    $result = json_encode($balas);
+//$result = ob_get_clean();
+
+    file_put_contents('./balasan.json', $result);
+
+
+    $client->replyMessage($balas);
+}
+?>
